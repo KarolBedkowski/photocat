789b0148a20f070a8626a05e5166614b0352f95e
k <k@a77d14a6-4f57-4b8c-96fb-12fa84005576>
2007-11-11 20:01:16 +0000

    pc/gui/wndmain.py, pc/gui/_dlgabout.py *uzupełnienie licencji
    git-svn-id: file:///home/k/jp/SVN/pc/trunk@19 a77d14a6-4f57-4b8c-96fb-12fa84005576

--- pc/gui/_dlgabout.py
+++ pc/gui/_dlgabout.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 # pylint: disable-msg=R0901, R0904
 """
- SAG - Simple Album Generator 3.x  (sag3)
- Copyright (c) Karol Będkowski, 2004, 2005, 2006
+ Photo Catalog v 1.0  (pc)
+ Copyright (c) Karol Będkowski, 2004-2007
 
- This file is part of Simple Album Generator (SAG)
+ This file is part of Photo Catalog
 
- SAG is free software; you can redistribute it and/or modify it under the
+ PC is free software; you can redistribute it and/or modify it under the
  terms of the GNU General Public License as published by the Free Software
  Foundation, version 2.
 
- SAG is distributed in the hope that it will be useful, but WITHOUT ANY
+ PC is distributed in the hope that it will be useful, but WITHOUT ANY
  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
