f167d9a55201dda3b472dd69228a10a3271d25bd
Karol Będkowski <karol.bedkowski@gmail.com>
2008-11-15 22:59:38 +0100

    dlgsearch: fix błąd przy wyszukiwaniu
    wywoływanie starej metody clear na thumbctrl

--- pc/gui/_dlgsearch.py
+++ pc/gui/_dlgsearch.py
@@ -585,7 +585,7 @@ class _DlgSearch(wx.Dialog):
 			self._bmp_preview.SetBitmap(wx.EmptyImage(1, 1).ConvertToBitmap())
 
 		else:
-			self._thumbctrl.clear()
+			self._thumbctrl.show_dir([])
 
 		self._btn_goto.Enable(False)
 		self._btn_properties.Enable(False)
