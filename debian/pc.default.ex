a555b2a886f37a4339a58e9b6e488a5d6f5675b5
Karol Będkowski <karol.bedkowski@gmail.com>
2008-11-15 22:49:44 +0100

    +katalog dla paczki

---
+++ debian/pc.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pc initscript
+# sourced by /etc/init.d/pc
+# installed at /etc/default/pc by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
