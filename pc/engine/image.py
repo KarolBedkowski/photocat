5f6de9e90864f385a9570e3fec37d31bbdcc1ff0
Karol Będkowski <karol.bedkowski@gmail.com>
2009-04-15 17:10:59 +0200

    gui: nie powiększanie małych obrazków
    pc.engine.image - max skala = 1

--- pc/engine/image.py
+++ pc/engine/image.py
@@ -97,7 +97,7 @@ def load_bitmap_from_item_with_size(item, width, height):
 	img_width	= img.GetWidth()
 	img_height	= img.GetHeight()
 
-	scale = min(float(width) / img_width, float(height) / img_height)
+	scale = min(float(width) / img_width, float(height) / img_height, 1)
 	img_width	= int(img_width * scale)
 	img_height	= int(img_height * scale)
 
