edc699901c3b3bc28a399bbbaf0a941b9a439a87
Karol Będkowski <karol.bedkowski@gmail.com>
2009-05-13 16:44:57 +0200

    storage: 3: tworzenie obiektów przez ObjectFactory

--- pc/storage/_storage_v3.py
+++ pc/storage/_storage_v3.py
@@ -28,18 +28,77 @@ __revision__	= '$Id$'
 
 import gzip
 import struct
+import logging
 try:
 	import cPickle as pickle
 except ImportError:
 	import pickle
 
-import logging
-_LOG = logging.getLogger(__name__)
-
-
 from storage_errors	import InvalidFileError, SaveFileError
 
 from pc.model import Disk, Catalog, FileImage, Directory, Tags
+
+
+_LOG = logging.getLogger(__name__)
+
+_CLASS_NAMES = dict(( (clazz.FV3_CLASS_NAME, clazz) for clazz in (Directory, Disk, FileImage) ))
+_CLASS_NAMES_TAGS = Tags.FV3_CLASS_NAME
+_CLASS_NAMES_DISK = Disk.FV3_CLASS_NAME
+
+
+
+class _ObjectFactory:
+	''' Klasa generująca obiekty na podstawie wczytywanego pliku '''
+
+	def __init__(self, catalog, objects):
+		self.catalog	= catalog
+		self.objects	= objects
+		self.disks		= catalog.disks
+
+
+	def create_object(self, oid, o_type_id, data, *argv, **kwarg):
+		''' obj_factory.create_object(oid, o_type_id, data, ...) -> object -- utworzenie obiektu.
+			Tworzy obiekt i dodaje go do struktury.
+
+			@param oid			id obiektu
+			@parem o_type_id	typ obiektu
+			@param data			dane do zdekodowania
+		'''
+		if not o_type_id in _CLASS_NAMES:
+			_LOG.warn('_ObjectFactory.create_object invalid class name: "%r"', o_type_id)
+			return None
+
+		klass	= _CLASS_NAMES[o_type_id]
+		data	= klass.decode3(pickle.loads(data))
+
+		data['catalog']	= self.catalog
+
+		# wstawienie obiektow nadrzędnych
+		parent = None
+		if 'parent_id' in data:
+			parent = self.objects.get(data['parent_id'])
+
+		data['parent'] = parent
+
+		disk_id = data.get('disk_id')
+		if disk_id and disk_id in self.objects:
+			data['disk'] = self.objects[disk_id]
+
+		# utworzenie klasy
+		self.objects[oid] = new_object = klass(oid, **data)
+
+		if parent:
+			parent.add_child(new_object)
+
+		else:
+			if o_type_id == _CLASS_NAMES_DISK:
+				self.disks.append(new_object)
+
+			else:
+				_LOG.warn('id without parent oid="%d"', oid)
+
+		return new_object
+
 
 
 class StorageV3:
@@ -56,7 +115,7 @@ class StorageV3:
 		try:
 			output_file = gzip.open(catalog.catalog_filename, 'w')
 			output_file.write(header + '\n')
-			
+
 			_LOG.debug('StorageV3.save file opened, header written')
 
 			oid, class_name, odata = catalog.tags_provider.encode3()
@@ -89,71 +148,37 @@ class StorageV3:
 
 	@staticmethod
 	def load(input_file, catalog, objects, filename):
-		class_names = dict(( (clazz.FV3_CLASS_NAME, clazz) for clazz in (Directory, Disk, FileImage) ))
-		class_names_tags = Tags.FV3_CLASS_NAME
-		class_names_disk = Disk.FV3_CLASS_NAME
-
 		oid = ''
 		tags_provider = catalog.tags_provider
+
+		obj_factory = _ObjectFactory(catalog, objects)
 
 		while True:
 			try:
 				oid, o_type_id, data_len, extra_len = StorageV3.__load_header(input_file)
 				if oid is None:
 					break
-				
+
 				data = input_file.read(data_len)
 				if len(data) < data_len:
 					_LOG.error('truncated file data (expect: %d, have: %d)', data_len, len(data))
 					raise InvalidFileError()
 
+				data_extra = None
 				if extra_len:
 					data_extra = input_file.read(extra_len)
 					if len(data_extra) < extra_len:
 						_LOG.error('truncated file extra (expect: %d, have: %d)', extra_len, len(data_extra))
 						raise InvalidFileError()
 
-				if o_type_id == class_names_tags:
+				if o_type_id == _CLASS_NAMES_TAGS:
 					tags_provider.tags = pickle.loads(data)
 					continue
 
-				if not o_type_id in class_names:
-					_LOG.warn('invalid class name: "%r"', o_type_id)
-					continue
-
-				klass	= class_names[o_type_id]
-				data	= klass.decode3(pickle.loads(data))
-
-				data['parent']	= None
-				data['catalog'] = catalog
-
-				# wstawienie obiektow nadrzędnych
-				if 'parent_id' in data:
-					data['parent']	= objects.get(data['parent_id'])
-
-				if 'disk_id' in data:
-					disk_id = data['disk_id']
-					if disk_id in objects:
-						data['disk']	= objects.get(disk_id)
-
-					elif o_type_id != class_names_disk:
-						_LOG.warn("no disk id=%d oid=%d", disk_id, oid)
-
-				# utworzenie klasy
-				objects[oid] = new_object = klass(oid, **data)
-
-				if data.get('parent') is None:
-					if o_type_id ==  class_names_disk:
-						catalog.disks.append(new_object)
-
-					else:
-						_LOG.warn('id without parent oid="%d"', oid)
-
-				else:
-					data['parent'].add_child(new_object)
-
-				# tagi
-				tags_provider.add_item(new_object)
+				new_object = obj_factory.create_object(oid, o_type_id, data, data_extra)
+				if new_object:
+					# tagi
+					tags_provider.add_item(new_object)
 
 			except Exception,err:
 				_LOG.exception('StorageV3.load(%s) oid=%d', filename, oid)
@@ -175,7 +200,6 @@ class StorageV3:
 			_LOG.warn('file truncated header: (except: %d, load: %d)', StorageV3.__HEADER_LEN, len(data))
 			raise InvalidFileError()
 
-		
 		h1, h2, h3, oid, o_type_id, data_len, extra_len = struct.unpack('cccLLLL', data)
 		if (h1, h2, h3) != StorageV3.__HEADER_HEAD_TUPLE:
 			_LOG.warn('header error: %r' % data)
