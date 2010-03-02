d4ea3e3c50b23d81e9435012d77f4f9f1fdafb4d
Karol Będkowski <karol.bedkowski@gmail.com>
2010-03-02 18:25:35 +0100

    po.pl: update

--- pc/version.py
+++ pc/version.py
@@ -32,14 +32,18 @@ DEVELOPERS = '''Karol Będkowski'''
 TRANSLATORS = '''Karol Będkowski'''
 COPYRIGHT = 'Copyright (C) Karol Będkowski, 2004-2010'
 LICENSE = _('''\
-PC is free software; you can redistribute it and/or modify it under the terms
-of the GNU General Public License as published by the Free Software Foundation,
-version 2.
+This program is free software: you can redistribute it and/or modify"
+it under the terms of the GNU General Public License as published by"
+the Free Software Foundation, either version 3 of the License, or"
+(at your option) any later version."
 
-PC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,"
+but WITHOUT ANY WARRANTY; without even the implied warranty of"
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+GNU General Public License for more details."
 
+You should have received a copy of the GNU General Public License"
+along with this program.  If not, see <http://www.gnu.org/licenses/>."
 
 Also please check LICENCE files from distribution.''')
 
